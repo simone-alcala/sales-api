463bb47e3704689ad3f311f74647d8ee8c745ddc
simone.alcala <simone.alcala@gmail.com>
2023-01-31 15:37:42 -0300

    Feat: Exception handler

--- src/main/java/com/treinamento/sales/services/UserService.java
+++ src/main/java/com/treinamento/sales/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.treinamento.sales.entities.User;
 import com.treinamento.sales.repositories.UserRepository;
+import com.treinamento.sales.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
