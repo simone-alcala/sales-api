5be6fdd2bc4afad134101182034f40cf99b673f1
simone.alcala <simone.alcala@gmail.com>
2023-02-01 17:15:26 -0300

    Feat:methodArgumentNotValid exception

--- src/main/java/com/treinamento/sales/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/treinamento/sales/controllers/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -32,12 +33,27 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
-	
 	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, HttpServletRequest request) {
+	public ResponseEntity<StandardError> dataIntegrityViolation(DataIntegrityViolationException e, HttpServletRequest request) {
 		String error = "Data integrity violation";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+		String error = "Bad request";
+		
+		String errorMessage = "";
+	    Object[] errorObjects = e.getDetailMessageArguments();
+
+	    for (int i = 1; i < errorObjects.length; i++) {     
+	        errorMessage += errorObjects[i];      
+	    }
+	    
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, errorMessage.isEmpty() ? e.getMessage() : errorMessage, request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 	
