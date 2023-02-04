33953b8b597b353603eced486e5c43ccc39e622e
simone.alcala <simone.alcala@gmail.com>
2023-02-04 15:19:56 -0300

    Chore: Add security dependencies

--- src/main/java/com/treinamento/sales/SalesApplication.java
+++ src/main/java/com/treinamento/sales/SalesApplication.java
@@ -2,9 +2,8 @@ package com.treinamento.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication()
 public class SalesApplication {
 
 	public static void main(String[] args) {
