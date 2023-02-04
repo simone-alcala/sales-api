e45a67cac7dd5ee4760347ed8908d973fc917764
simone.alcala <simone.alcala@gmail.com>
2023-02-04 15:23:53 -0300

    Feat: Add auth exception

--- src/main/java/com/treinamento/sales/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/treinamento/sales/controllers/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,9 @@ import java.time.Instant;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.treinamento.sales.services.exceptions.DatabaseException;
 import com.treinamento.sales.services.exceptions.ResourceNotFoundException;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
@@ -38,6 +43,46 @@ public class ResourceExceptionHandler {
 		String error = "Data integrity violation";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(UsernameNotFoundException.class)
+	public ResponseEntity<StandardError> usernameNotFound(UsernameNotFoundException e, HttpServletRequest request) {
+		String error = "Username not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<StandardError> badCredentials(BadCredentialsException e, HttpServletRequest request) {
+		String error = "Bad credentials";
+		HttpStatus status = HttpStatus.FORBIDDEN;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, "Invalid user/password", request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(SignatureException.class)
+	public ResponseEntity<StandardError> signatureException(SignatureException e, HttpServletRequest request) {
+		String error = "JWT signature does not match";
+		HttpStatus status = HttpStatus.FORBIDDEN;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(ExpiredJwtException.class)
+	public ResponseEntity<StandardError> expiredJwt(ExpiredJwtException e, HttpServletRequest request) {
+		String error = "Expired Jwt";
+		HttpStatus status = HttpStatus.FORBIDDEN;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(AuthenticationException.class)
+	public ResponseEntity<StandardError> authenticationException(AuthenticationException e, HttpServletRequest request) {
+		String error = "Bad credentials";
+		HttpStatus status = HttpStatus.FORBIDDEN;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, "Invalid credentials", request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 	
